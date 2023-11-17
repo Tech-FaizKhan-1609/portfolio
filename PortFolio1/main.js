86e46d8ca41006b23cd3b5681a9f106a9c057256
KHAN MOHAMMAD FAIZ <133863738+khanfaiz1609@users.noreply.github.com>
2023-11-17 19:53:48 +0530

    Add files via upload

---
+++ PortFolio1/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Full Stack Developer" , "Student"],
+    typeSpeed: 100,
+    backSpeed: 90,
+    backDelay: 100,
+    loop: true
+  });
+ 
